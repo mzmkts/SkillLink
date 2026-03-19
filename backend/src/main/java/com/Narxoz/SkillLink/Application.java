1ac9711307c5c5363a2810af3fb0584865faa19f
mzmkts <glemillov@gmail.com>
2026-03-19 23:08:44 +0500

    mod: now properties can understand dotenv

--- backend/src/main/java/com/Narxoz/SkillLink/Application.java
+++ backend/src/main/java/com/Narxoz/SkillLink/Application.java
@@ -1,5 +1,6 @@
 package com.Narxoz.SkillLink;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("PG_USERNAME", dotenv.get("PG_USERNAME"));
+        System.setProperty("PG_PASSWORD", dotenv.get("PG_PASSWORD"));
+        System.setProperty("PG_URL", dotenv.get("PG_URL"));
 		SpringApplication.run(Application.class, args);
 	}
 
