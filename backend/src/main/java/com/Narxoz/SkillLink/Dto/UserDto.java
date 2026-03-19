682369571d752b0bc770c3f54843b98fe7d9afca
mzmkts <glemillov@gmail.com>
2026-03-19 22:07:26 +0500

    feat: added dto for users

---
+++ backend/src/main/java/com/Narxoz/SkillLink/Dto/UserDto.java
@@ -0,0 +1,14 @@
+package com.Narxoz.SkillLink.Dto;
+
+import com.Narxoz.SkillLink.Model.Role;
+
+import java.util.List;
+
+public class UserDto {
+    private String userIdDto;
+    private String firstNameDto;
+    private String lastNameDto;
+    private String passwordDto;
+    private String emailDto;
+    private List<Role> roles;
+}
