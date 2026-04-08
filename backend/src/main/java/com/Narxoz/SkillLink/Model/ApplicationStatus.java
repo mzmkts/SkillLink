dec405fb0110f4a426f664c83d63f487288a7bf5
mzmkts <glemillov@gmail.com>
2026-04-08 15:44:51 +0500

    feat: added Application and project mapper and now enums is separate class

---
+++ backend/src/main/java/com/Narxoz/SkillLink/Model/ApplicationStatus.java
@@ -0,0 +1,7 @@
+package com.Narxoz.SkillLink.Model;
+
+public enum ApplicationStatus {
+    PENDING,
+    ACCEPTED,
+    REJECTED
+}
---
+++ backend/src/main/java/com/Narxoz/SkillLink/Model/ProjectStatus.java
@@ -0,0 +1,7 @@
+package com.Narxoz.SkillLink.Model;
+
+public enum ProjectStatus {
+    OPEN,
+    IN_PROGRESS,
+    CLOSED
+}
