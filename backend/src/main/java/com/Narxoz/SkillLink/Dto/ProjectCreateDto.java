b5d2b85f5183b922beac2d51544f9b91e414c577
mzmkts <glemillov@gmail.com>
2026-04-08 15:35:48 +0500

    feat: added Application and project dtos

--- backend/src/main/java/com/Narxoz/SkillLink/Dto/ProjectCreateDto.java
+++ backend/src/main/java/com/Narxoz/SkillLink/Dto/ProjectCreateDto.java
@@ -1,5 +1,14 @@
 package com.Narxoz.SkillLink.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProjectCreateDto {
     private String title;
     private String description;
--- backend/src/main/java/com/Narxoz/SkillLink/Dto/ProjectResponseDto.java
+++ backend/src/main/java/com/Narxoz/SkillLink/Dto/ProjectResponseDto.java
@@ -1,5 +1,15 @@
 package com.Narxoz.SkillLink.Dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProjectResponseDto {
     private Long id;
     private String title;
